21d417c947183ae3226a44434b881bf9ec96acfc
Javier Pérez Ruiz <javier.perez@davinchi.es>
2018-03-08 11:38:03 +0100

    fix: Removed index.ts, causes yeoman to not found haztivity generator other: Added LICENSE file

--- generators/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @module generatorHaztivity
- * @preferred
- *//** */
-export * from "./BaseGenerator";
-export * from "./page";
-export * from "./sco";
-export * from "./course";
-export * from "./app";
